0f7d81c023893c3f25c2769487e9593b24842df2
Keedjud <inoewenger@gmail.com>
2026-02-23 16:03:23 +0100

    Add switch en fonction de l'url

--- src/index.js
+++ src/index.js
@@ -7,23 +7,29 @@ const artistList = document.querySelector('.artist-list')
 
 afficherSectionArtistes()
 
-// const hashSplité = window.location.hash.split(‘/‘)
-// // si le premier élément est artiste, on est dans la gestion des artistes...
-// switch (hashSplité[0]) {
-//     case ‘#artists’:
-//         // est-ce que le deuxième élément retourne quelque chose ? Et donc n’est pas undefined ? Oui? 
-//         // Alors il y a un id et on affiche cet artiste
-//         if (hashSplité[1]) {
-//             afficherChansonsArtiste(hashSplité[1])
-//         }
-//         else {
-//             afficherArtistes()
-//         }
-//         break;
-//     case ‘#player’: 
+const hashSplité = window.location.hash.split('/')
+switch (hashSplité[0]) {
+    case '':
+        afficherHome()
+        break;
 
-//         break;
-// }
+    case '#artists':
+        if (hashSplité[1]) {
+            afficherSectionMusiques(hashSplité[1])
+        }
+        else {
+            afficherSectionArtistes()
+        }
+        break;
+
+    case '#player':
+
+        break;
+
+    case '#favorites':
+
+        break;
+}
 
 artistList.addEventListener('click', (event) => {
     const artistCover = event.target.closest('artist-cover')
